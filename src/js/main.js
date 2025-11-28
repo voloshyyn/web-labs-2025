9959349ee9d0760e6f1836ac38101caac9b46384
Andrii Voloshyn <25062006vas@gmail.com>
2025-11-29 01:32:33 +0200

    Write the first page (index) in SCSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import "./scss/main.scss";
+import "../scss/main.scss";
 import { Store } from "./core/store.js";
 import { api } from "./api/http.js";
 import { endpoints } from "./api/endpoints.js";
